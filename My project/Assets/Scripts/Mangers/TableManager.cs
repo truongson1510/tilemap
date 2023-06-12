73dd3b36d8f50daa13a2e6a36a2e85075009dc2b
turongson <50595413+truongson1510@users.noreply.github.com>
2023-06-12 21:44:03 +0700

    * deliver commit *

--- My project/Assets/Scripts/Mangers/TableManager.cs
+++ My project/Assets/Scripts/Mangers/TableManager.cs
@@ -4,29 +4,39 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class TableManager : MonoBehaviour
 {
     public static TableManager Instance;
+    [SerializeField] GameObject[] allIngredient;
 
-    [SerializeField] GameObject[]       allIngredient;
-    [SerializeField] List<FoodRecipe>   recipes = new();
+    [SerializeField] List<FoodRecipe> recipes = new List<FoodRecipe>();
 
-    [SerializeField] int                selectedFood;
-    [SerializeField] int                selectedTime;
+    [SerializeField] int            selectedFood;
+    [SerializeField] int            selectedTime;
 
-    [SerializeField] GameObject         foodtoShow;
-    [SerializeField] GameObject         player;
-    [SerializeField] GameObject         playerPoint;
-    [SerializeField] Animator           playerAnimator;
+    [SerializeField] GameObject     foodtoShow;
 
-    [SerializeField] Vector3            initialPos;
+    [SerializeField] GameObject     player;
+    [SerializeField] GameObject     playerPoint;
+    [SerializeField] Animator       playerAnimator;
 
-    [SerializeField] bool               playerReached = false;
-    [SerializeField] bool               foodServed = false;
+    [SerializeField] GameObject     waiter;
+    [SerializeField] GameObject     waiterPoint;
+    [SerializeField] Animator       waiterAnimator;
 
-    [SerializeField] TMP_Text           timeText;
-    [SerializeField] GameObject         timmer;
-    [SerializeField] SpriteRenderer     order;
+    [SerializeField] GameObject     score;
+    [SerializeField] Vector3        initialPos;
+    [SerializeField] Vector3        waterInitialPos;
+
+
+    [SerializeField] bool           playerReached = false;
+    [SerializeField] bool           foodServed = false;
+
+    [SerializeField] TMP_Text timeText;
+    [SerializeField] GameObject timmer;
+    [SerializeField] SpriteRenderer order;
+
 
     int rand = 0;
     bool lowPlayed;
@@ -35,42 +45,44 @@ public class TableManager : MonoBehaviour
     {
         Instance = this;
     }
-
     // Start is called before the first frame update
     void Start()
     {
         initialPos = player.transform.position;
+        waterInitialPos = waiter.transform.position;
+
         InitializeTable();
     }
 
     void InitializeTable()
     {
-        lowPlayed       = false;
-        playerReached   = false;
-        foodServed      = false;
+        playerReached = false;
+        foodServed = false;
 
         foodtoShow.SetActive(false);
         timmer.SetActive(false);
 
         player.transform.position = initialPos;
+        waiter.transform.position = waterInitialPos;
 
-        rand            = Random.Range(0, 2);
-        selectedFood    = Random.Range(0, 4);
-
-        if (rand==0)
-        { selectedTime = 30; }
+        rand = Random.Range(0, 2);
+        selectedFood = Random.Range(0, 4);
+        if (rand == 0)
+        {
+            selectedTime = 30;
+        }
         else
-        { selectedTime = 60; }
-
-        timeText.text=selectedTime.ToString();
+        {
+            selectedTime = 60;
+        }
+        timeText.text = selectedTime.ToString();
         StartCoroutine(MovePlayer());
     }
 
     IEnumerator MovePlayer()
     {
         playerAnimator.SetTrigger("Move");
-
-        while (player.transform.position!=playerPoint.transform.position)
+        while (player.transform.position != playerPoint.transform.position)
         {
             player.transform.position = Vector3.MoveTowards(player.transform.position, playerPoint.transform.position, 5 * Time.fixedDeltaTime);
             yield return new WaitForSeconds(0.01f);
@@ -82,60 +94,56 @@ public class TableManager : MonoBehaviour
         playerAnimator.SetTrigger("Sit");
 
         if (order != null) { order.sprite = recipes[selectedFood].foodImage; }
-        
-        player.transform.GetChild(0).gameObject.SetActive(true);
 
+        player.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = recipes[selectedFood].foodImage;
+        player.transform.GetChild(0).gameObject.SetActive(true);
         FoodManager.Instance.TurnOnButtons();
         StartCoroutine(timeRoutine());
+
         yield return new WaitForSeconds(2f);
         player.transform.GetChild(0).gameObject.SetActive(false);
     }
 
     IEnumerator timeRoutine()
     {
-        while ((selectedTime>0) && !foodServed)
+        while ((selectedTime > 0) && foodServed == false)
         {
             yield return new WaitForSeconds(1);
 
             selectedTime--;
             timeText.text = selectedTime.ToString();
 
-            if(selectedTime < 3 && !foodServed && !lowPlayed)
+            if (selectedTime < 3 && !foodServed && !lowPlayed)
             {
                 AudioManager.Instance.PlayOneShot(Sound.timeLow);
                 lowPlayed = true;
             }
 
-            if ((selectedTime<=0)&& !foodServed)
+            if ((selectedTime <= 0) && foodServed == false)
             {
                 AudioManager.Instance.PlayOneShot(Sound.failToServe);
 
                 StopAllCoroutines();
                 LiveManager.instance.DecreaseLive();
                 InitializeTable();
-            }    
+            }
         }
     }
     public void CheckFood(GameObject gameObject)
     {
-        if (playerReached)
+        if (playerReached == true)
         {
             if (recipes[selectedFood].ingredients[recipes[selectedFood].count].gameObject == gameObject)
             {
                 recipes[selectedFood].count++;
                 if (recipes[selectedFood].count == recipes[selectedFood].ingredients.Length)
                 {
-                    foodServed = true;
-
                     AudioManager.Instance.PlayOneShot(Sound.doneCooking);
 
-                    foodtoShow.GetComponent<SpriteRenderer>().sprite = recipes[selectedFood].foodImage;
-                    foodtoShow.SetActive(true);
                     playerReached = false;
+                    foodServed = true;
                     StopAllCoroutines();
                     StartCoroutine(ResetTable());
-                    recipes[selectedFood].count = 0;
-                    ScoreManager.Instance.IncreaseScore();
                 }
             }
             else
@@ -147,7 +155,19 @@ public class TableManager : MonoBehaviour
 
     IEnumerator ResetTable()
     {
-        yield return new WaitForSeconds(3);
+        while (waiter.transform.position != waiterPoint.transform.position)
+        {
+            waiter.transform.position = Vector3.MoveTowards(waiter.transform.position, waiterPoint.transform.position, 5 * Time.fixedDeltaTime);
+            yield return new WaitForSeconds(0.01f);
+        }
+        foodtoShow.GetComponent<SpriteRenderer>().sprite = recipes[selectedFood].foodImage;
+        foodtoShow.SetActive(true);
+        recipes[selectedFood].count = 0;
+        //score.SetActive(true);
+        ScoreManager.Instance.IncreaseScore();
+        player.transform.GetChild(0).gameObject.SetActive(false);
+        yield return new WaitForSeconds(2f);
+        //score.SetActive(false);
         InitializeTable();
     }
 }
